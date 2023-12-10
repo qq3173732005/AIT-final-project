e051d53f496a24b61d93b090cff38e14aaee725e
qq3173732005 <76537620+qq3173732005@users.noreply.github.com>
2023-12-10 16:07:49 -0500

    fix update problem for adding course

--- app/api/courses/route.js
+++ app/api/courses/route.js
@@ -5,7 +5,7 @@ import CourseModel from "@/models/course";
 import mongoose from "mongoose";
 import { NextResponse } from "next/server";
 
-export const dynamic  = 'force-dynamic';
+export const revalidate  = 0;
 
 export async function POST(request) {
     const {name} = await request.json();
@@ -18,6 +18,7 @@ export async function POST(request) {
 export async function GET() {
     await connectMongoDB();
     const courses = await CourseModel.find();
+    console.log('getting');
     return NextResponse.json({courses}, {status: 200});
 };
 
