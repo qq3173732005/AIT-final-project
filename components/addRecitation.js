20bf234f8b7725d2b4c0db810f2b823669d8fe94
qq3173732005 <76537620+qq3173732005@users.noreply.github.com>
2023-12-10 22:42:02 -0500

    moved meeting time form outside of recitation form

--- components/addRecitation.js
+++ components/addRecitation.js
@@ -2,12 +2,14 @@ import AddMeetingTime from "./addMeetingTime";
 
 export default function AddRecitation() {
     return (
-        <form className="text-white bg-gray text-xs w-11/12 py-1 pl-1">
-            <span>Recitation/Lab Number: </span>
-            <input type="text"/>
+        <div className="text-black bg-gray text-xs w-11/12 py-1 pl-1">
+            <form>
+                <span>Recitation/Lab Number: </span>
+                <input type="text"/>
+                <input type="submit" value="Add Recitation" className="bg-black text-antiquewhite px-1"/>
+                <input type="submit" value="Cancel" className="bg-black text-antiquewhite px-1 ml-2"/>
+            </form>
             <AddMeetingTime/>
-            <input type="submit" value="Add Recitation" className="bg-black text-antiquewhite px-1"/>
-            <input type="submit" value="Cancel" className="bg-black text-antiquewhite px-1 ml-2"/>
-        </form>
+        </div>
     );
 };
