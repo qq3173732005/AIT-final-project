21565c9346a42e84c38e2f432e8735b32f84135b
qq3173732005 <76537620+qq3173732005@users.noreply.github.com>
2023-12-10 22:44:50 -0500

    add section

--- components/addSection.js
+++ components/addSection.js
@@ -1,13 +1,45 @@
+'use client';
+
+import { useState } from "react";
 import AddMeetingTime from "./addMeetingTime";
 
-export default function AddSection () {
+export default function AddSection ({course_id}) {
+    const [number, setNumber] = useState('');
+    const meetingTimes = [];
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const res = await fetch(`${window.location.toString()}api/sections`, {
+                method: 'POST',
+                headers: {
+                    'Content-type': 'application/json'
+                },
+                body: JSON.stringify({course_id, number, meetingTimes})
+            });
+            if(!res.ok) {
+                throw new Error("Failed to create section.");
+            }
+            window.location.reload();
+            setNumber('');
+        }
+        catch(error) {
+            console.log(error);
+        }
+    }
     return (
-        <form className="text-white text-xs bg-gray w-11/12 py-1 pl-1">
-            <span>Section Number: </span>
-            <input type="text"/>
+        <div className="text-black text-xs bg-gray w-11/12 py-1 pl-1">
+            <form onSubmit={handleSubmit}>
+                <span>Section Number: </span>
+                <input
+                    type="text"
+                    onChange={(e) => setNumber(e.target.value)}
+                    placeholder="Section Number"
+                    value={number}
+                />
+                <input type="submit" value="Add Section" className="bg-black text-antiquewhite px-1"/>
+                <input type="submit" value="Cancel" className="bg-black text-antiquewhite px-1 ml-2"/>
+            </form>
             <AddMeetingTime/>
-            <input type="submit" value="Add Section" className="bg-black text-antiquewhite px-1"/>
-            <input type="submit" value="Cancel" className="bg-black text-antiquewhite px-1 ml-2"/>
-        </form>
+        </div>
     );
 };
--- components/sectionlist.js
+++ components/sectionlist.js
@@ -3,11 +3,15 @@ import AddSection from "./addSection";
 import SectionModel from "@/models/section";
 import Section from "./section";
 
+export const revalidate  = 0;
+export const dynamic = 'force-dynamic';
+export const fetchCache = 'force-no-store';
+
 export default async function SectionList({course_id}) {
     const sections = await getServerSideProps(course_id);
     return (
         <div className="pl-12">
-            <AddSection/>
+            <AddSection course_id={course_id}/>
             {sections.map((section) => (
                 <Section key={section._id} course_id={course_id} number={section.number} meetingTimes={section.meetingTimes} _id={section._id}/>
             ))}
