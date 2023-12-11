d9b6a4e8587ace6b61485df65e8b413fa63c4d83
qq3173732005 <76537620+qq3173732005@users.noreply.github.com>
2023-12-10 22:44:22 -0500

    formatting

--- app/page.js
+++ app/page.js
@@ -3,6 +3,7 @@ import Coursebar from '@/components/coursebar';
 export const revalidate  = 0;
 export const dynamic = 'force-dynamic';
 export const fetchCache = 'force-no-store';
+
 export default function Home() {
   return (
     <body>
